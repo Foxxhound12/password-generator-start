151e6f513479adea6adf1e677e371b23dde4fb21
Foxxhound12 <128701923+Foxxhound12@users.noreply.github.com>
2023-03-31 20:21:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -38,3 +38,42 @@ random.shuffle(password)
 #oder
 final_password = str.join("", password) #wäre auch mit for loop möglich final_password="", for item in password: final_password += item (string concatenation)
 print(final_password)
+
+#v2:
+# Password Generator Project
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+# Eazy Level - Order not randomised:
+# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+
+# Hard Level - Order of characters randomised:
+# e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+password = []
+for letter in range(nr_letters):
+    l = random.randint(0, len(letters) - 1) #oder überhaupt password.append(random.choice(letters))
+    password.append(letters[l])
+
+for symbol in range(nr_symbols):
+    s = random.randint(0, len(symbols) - 1)
+    password.append(symbols[s])
+
+
+for number in range(nr_numbers):
+    n = random.randint(0, len(numbers) - 1)
+    password.append(numbers[n])
+
+random.shuffle(password)
+
+print("Your password is: "+str.join("", password))
